8bd31db8cd336564f79d556de5b76cf6ac2d406f
kamil <kamil@example.com>
2013-04-16 22:13:50 +0800

    First commit for forest skele

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forest::Application.config.secret_token = '5971424ec8f37e94f006666fa5e471bdec657c4704bff72eadb2c0d632f09cd7f75a954525be8515c163a88d6078670acc94ee86662ba7aa6c74729b065721df'
